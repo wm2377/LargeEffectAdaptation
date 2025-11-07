9a26170fc2a867f401ec792f9936a557272248be
wm2377 <37453170+wm2377@users.noreply.github.com>
2025-11-07 11:47:14 -0500

    Improve comments
    Refactor comments for clarity

--- simulations_10042023_phase_space_simulations_alt_parameterizaation.py
+++ simulations_10042023_phase_space_simulations_alt_parameterizaation.py
@@ -20,8 +20,10 @@ from scipy.integrate import quad
 import time
 import gzip
 
+# The goal of this file is to run simplified all allele simulations in parallel
 def main():
-    # get args
+    # First we get the args from snakemake. 
+    # If the args are given in total variance and p, instead of sigma2 and 2NU, we make the necessary transformation
     args = snakemake.params
     sdist = args.sdist
     
@@ -32,25 +34,30 @@ def main():
         args.N2U = p*total_variance/4
     except:
         pass
-    
-    if args.shift > 100:
+    # If the shift size is too large, we ignore it.
+    # Generate a null output, so snakemake thinks it did something
+    if args.shift > np.sqrt(args.N*2):
         generate_null_output(args)
         return
     else:
+        # Try and see if we have completed any of these simulations already
         trials_completed = get_trials_completed(args)
         trials_completed, p = simulations_with_given_parameters(args,trials_completed)
         output_results(trials_completed,args)
-    
+
+# Makes a null output for when we don't actually want to simulate these parameter combinations
 def generate_null_output(args):
     trials_completed = {args.N2U:{args.shift:{args.sigma2:[np.nan]*args.trials}}}
     output_results(trials_completed,args)
     
-
+# Check if the output already exists and see how many replicates we have already ran
 def get_trials_completed(args):
 
-    if os.path.exists(snakemake.output.results_file_path) and not np.isclose(args.N2U,0.5,rtol=0.01):
+    # If output exists, load the results
+    if os.path.exists(snakemake.output.results_file_path):
         with gzip.open(snakemake.output.results_file_path, 'rb') as fout:
             trials_completed = pickle.load(fout)
+    # if output does not exist, start a new output dict
     else:
         trials_completed = {}
         if args.N2U not in trials_completed.keys():
@@ -64,20 +71,20 @@ def get_trials_completed(args):
 
     return trials_completed
 
+# Runs jobs_per_executor number of simulations, this is the function passed to the parallelization command
 def run_simulation(idk):
     i,args = idk
     results = []
     for i in range(args.jobs_per_executor):
-        Va = args.sigma2 + 4*args.N2U
-        p = 4*args.N2U/Va
-        if 300/((1-p)**0.71) < Va:
-            results.append([])
-        else:
-            simulation_t = Simulation(N=args.N, sdist=args.sdist, N2U=args.N2U, sigma2=args.sigma2, shift=args.shift,output_full=False)
-            simulation_t.run_simulation()
-            results.append((simulation_t.stats['fixations']))
+        # Initialize and run the simulation
+        # Here, we only record the fixations that occured
+        # This can be changed to get other metrics
+        simulation_t = Simulation(N=args.N, sdist=args.sdist, N2U=args.N2U, sigma2=args.sigma2, shift=args.shift,output_full=False)
+        simulation_t.run_simulation()
+        results.append((simulation_t.stats['fixations']))
     return results
 
+# Calculate the current estimate forr the probability of any large effect reaching fixation
 def calc_p(trials_completed_sigma2):
 
     if len(trials_completed_sigma2) == 0:
@@ -87,7 +94,9 @@ def calc_p(trials_completed_sigma2):
     p_ste = np.sqrt(p*(1-p)/len(trials_completed_sigma2))
     return p,p_ste,num_fixations
 
+# Does the parallelization
 def simulations_with_given_parameters(args,trials_completed):
+    # Get the current estimate for the probability of any large effect reaching fixation and how many replicates have already been run
     try:
         p,p_ste,num_fixations = calc_p(trials_completed_sigma2=trials_completed[args.N2U][args.shift][args.sigma2])
         t = len(trials_completed[args.N2U][args.shift][args.sigma2])
@@ -95,6 +104,9 @@ def simulations_with_given_parameters(args,trials_completed):
         trials_completed = {args.N2U: {args.shift: {args.sigma2: trials_completed[2*args.N2U][args.shift][args.sigma2]}}}
         p,p_ste,num_fixations = calc_p(trials_completed_sigma2=trials_completed[args.N2U][args.shift][args.sigma2])
         t = len(trials_completed[args.N2U][args.shift][args.sigma2])
+
+    # Runs the simulations, splitting the tasks over max_executor "nodes"
+    # Import to outout results every time, so we have them in case this crashes/the job gets cancelled
     t_run = 0
     print(t)
     sys.stdout.flush()
@@ -119,6 +131,7 @@ def simulations_with_given_parameters(args,trials_completed):
     
     return trials_completed, p
 
+# Stores the results
 def output_results(simulation_results,args):
     results_file_path = snakemake.output.results_file_path
     with gzip.open(results_file_path, 'wb') as fout:

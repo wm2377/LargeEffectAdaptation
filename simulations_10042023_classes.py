14284d5b7532ef531d36e6866778dd1a45dbac57
wm2377 <37453170+wm2377@users.noreply.github.com>
2025-11-07 11:56:39 -0500

    Enhance documentation in Simulation class
    Added comments to clarify class functionality and methods.

--- simulations_10042023_classes.py
+++ simulations_10042023_classes.py
@@ -8,6 +8,9 @@ from scipy.optimize import root
 from copy import deepcopy as dc
 from generate_segregating_mutations import generate_alleles
 
+# Defines the classes for the simplified all-allele simulations
+
+# The first class is the simulation itself
 class Simulation:
 
     def __init__(self, N, sdist, N2U, sigma2, shift, tracking_time = 50, output_full = True, store_mutation_trajectories=False,stop_time=1e4):
@@ -36,6 +39,7 @@ class Simulation:
         for name in self.stat_names:
             self.stats[name] = []
 
+    # Some functions to define the steady state distribution of allele frequencies
     def variance_star(self, S, x):
         return 2 * S * x * (1 - x)
 
@@ -51,12 +55,17 @@ class Simulation:
             else:
                 return value
 
+    # Determine how many segregating sites we expect
+    # This is the integral over the sojourn time times 2NU
     def total_n(self):
         a = quad(
             lambda S: quad(lambda x: self.folded_sojourn_time(S=S, x=x), 0, 1 / 2, points=[1 / (2 * self.N)])[
                           0] * self.sdist.pdf(S), self.sdist.ppf(0.0000001), self.sdist.ppf(0.99999))[0]
         return a * self.N2U
 
+    ###### I think I don't use this anymore, but I leave it here in case I want to be able to refer to it with just the simulation object
+    # Find the initial frequency based on a random starting value.
+    # Must be between 0 and 0.5
     def cumulant(self, S, y):
         if y >= 0.5:
             return 1
@@ -66,6 +75,7 @@ class Simulation:
         bottom = quad(lambda x: self.folded_sojourn_time(S=S, x=x), 0, 1 / 2, points=[1 / (2 * self.N)])[0]
         return top / bottom
 
+    # Run the root finding algorithm to generate a random initial frequency
     def get_frequency(self, S):
         y0 = np.random.random()
         x = root(lambda y: self.cumulant(S=S, y=y) - y0, 1 / (2 * self.N)).x[0]
@@ -73,23 +83,26 @@ class Simulation:
             y0, x = self.get_frequency(S)
         return y0, x
 
+    # Run the root finding algorithm to get a given initial frequency
     def get_given_frequency(self, S, y0):
         return root(lambda y: self.cumulant(S=S, y=y) - y0, 1 / (2 * self.N)).x[0]
 
     def generate_segregating_mutations():
         pass
-
+    ########
+    
+    # Generate the set of alleles segregating at the begining of the shift
     def initiate_mutations(self, n):
-        
-
+        # The number of alleles is poisson distributed around twice the expectation (because of aligned and opposing)
         n_realized = np.random.poisson(2 * n)
+        # We need the probability of segregating for the next step of generating the effect sizes
         prob_segregating = quad(lambda S: quad(lambda x: self.folded_sojourn_time(S=S,x=x),0,1/2,points=[1/(2*self.N)])[0]*self.sdist.pdf(S),self.sdist.ppf(0),self.sdist.ppf(0.9999))[0]
+        # get the effect sizes. I offloaded all this into a seperate file, because I use some precalculated files
         chosen_muts = generate_alleles(n=n_realized,prob_segregating=prob_segregating,N=self.N,sdist=self.sdist)  
 
+        # add the variants to the list of mutations
         mutations = []
         for (x, S, y0) in chosen_muts:
-            # S = self.sdist.rvs()
-            # y0, x = self.get_frequency(S=S)
             mutations.append(Mutation(x=x, a=np.sqrt(S), y0=y0, t=0, store=self.store_mutation_trajectories))
 
         self.mutations = mutations
@@ -197,7 +210,7 @@ class Simulation:
         opposing_interval = 0
         negative_interval = 0
         positive_interval = 0
-        while t < self.stop_time or np.abs(d) > 2:# or not self.all_segregating_extinct:
+        while t < self.stop_time or np.abs(d) > 1:# or not self.all_segregating_extinct:
 
             # update allele frequencies and remove mutations that went extinct or fixed. Mutations that we want to store
             # or any that fix are stored within these functions
@@ -216,9 +229,6 @@ class Simulation:
 
             # update the distance
             d = self.update_distance_trajectory(d=d, change_in_d=change_in_d)
-
-            # if t > 5000 and not self.all_segregating_extinct:
-            #    self.check_all_segregating_extinct()
                 
             # in-depth output if we want it
             if self.output_full:
@@ -245,6 +255,7 @@ class Simulation:
 
             t += 1
 
+    # Some functions for defining different metrics
     def update_second_moment(self):
         self.stats['second_moment'].append(sum([mut.calculate_second_moment() for mut in self.mutations]))
 
@@ -290,6 +301,7 @@ class Simulation:
 
         return negative, positive, aligned, opposing
 
+    # This initializes the simulation and then runs it
     def run_simulation(self):
         # print('running simulation')
         # sys.stdout.flush()
@@ -301,7 +313,7 @@ class Simulation:
         # sys.stdout.flush()
         self.recursion()
 
-
+# The second class defines the mutations
 class Mutation:
 
     def __init__(self, x, a, y0, t, store=True):
